3a6055ec7bc1454e7bcf3ad6357012a2a09f8610
dogtrick <dogtrick@gmail.com>
2024-08-26 20:40:01 +0800

    commit

--- main.hs
+++ main.hs
@@ -5,27 +5,29 @@ import Data.Char (toLower)
 data Trie = Node Int (M.Map Char Trie)
     deriving Show
 
+splitChars :: [Char]
+splitChars = [' ', ',', '.', '!', '?', ';', ':', '\n', '/', '\\', '-', '_', '"', '(', ')', '[', ']', '{', '}', '<', '>', '&', '~', '|', '#', '@', '`']
+
 insertToTrie :: String -> Trie -> Trie
 insertToTrie "" (Node i m) = Node (i + 1) m
 insertToTrie (x:xs) (Node i m) = case M.lookup x m of 
                                    Nothing -> Node i $ M.insert x (insertToTrie xs $ Node 0 M.empty) m
-                                   Just n -> Node i $ M.insert x (insertToTrie xs n) m
+                                   Just n -> Node i $ M.adjust (insertToTrie xs) x m
 
 contextToTrie :: String -> Trie
 contextToTrie = aux (Node 0 M.empty)
-    where s = [' ', ',', '.', '!', '?', ';', ':', '\n', '/', '\\', '-', '_', '"', '(', ')', '[', ']', '{', '}', '<', '>', '&', '~', '|', '#', '@', '`'] 
-          popWord y "" = (reverse y, "")
-          popWord y (x:xs) = if x `elem` s then (reverse y, xs)
-                                           else popWord (toLower x : y) xs
+    where popWord y "" = (reverse y, "")
+          popWord y (x:xs) = if x `elem` splitChars then (reverse y, xs)
+                                                    else popWord (toLower x : y) xs
           aux t "" = t
           aux t s = let (x, s') = popWord "" s
-                     in if L.null x then aux t s'
-                                    else aux (insertToTrie x t) s'
+                     in if x == "" then aux t s'
+                                   else aux (insertToTrie x t) s'
 
 trieToDict :: Trie -> [(String, Int)]
 trieToDict (Node i m) = let ks = M.keys m
-                            acc = [("", i) | i > 0]
-                         in L.foldr (\k a -> let (Just t) = M.lookup k m in a ++ L.map (\(s, i') -> (k:s, i')) (trieToDict t)) acc ks
+                         in L.foldr (\k a -> let (Just t) = M.lookup k m 
+                                              in a ++ L.map (\(s, i') -> (k:s, i')) (trieToDict t)) [("", i) | i > 0] ks
 
 formatOutput :: (String, Int) -> IO ()
 formatOutput (w, n) = let l = length w
